3c4f4097b54927f1c82c21eacbb969486c334268
Tim Cocu <timcocu@gmail.com>
2021-03-06 21:36:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ld-phaser
+Basic template application for using Phaser 3 with  TypeScript. 
+
+
+```
+npm install && npm run dev
+```
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'ld-phaser',
+  version: '1.0',
+  width: 1280,
+  height: 720,
+  backgroundColor: 0x222222,
+  type: Phaser.AUTO
+};
